39260836c36971676f0bd12c11712f26882993a3
ncobb01 <ncobb01@gmail.com>
2018-04-17 22:17:48 -0700

    two pages connected

--- app/src/main/java/com/example/ncobb01/hw_ad340/MainActivity.java
+++ app/src/main/java/com/example/ncobb01/hw_ad340/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.app.Activity;
 
 import android.widget.EditText;
 import android.widget.TextView;
@@ -25,23 +26,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LoginButton();
-    }
 
 
-    public void LoginButton(){
         btnlogin = (Button) findViewById(R.id.form_submit);
+
+        btnlogin.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, ConfirmationPage.class));
+            }
+        });
+
+
+
+
         age = (EditText) findViewById(R.id.form_age);
        birthDate = (EditText) findViewById(R.id.form_dob);
-       birthDate = (EditText) findViewById(R.id.form_username);
+       username = (EditText) findViewById(R.id.form_username);
+
+        //mText.setText("Welcome "+mEdit.getText().toString()+"!");
+
+//
+//        btnlogin.setOnClickListener(new View.OnClickListener() {
+//
+//            public void onClick( new View.OnClickListener)
+//                                    }
+//
+//            LoginButtonClick(); }
 
 
-    }
+//            public void LoginButtonClick() {
+//
+//        int ageInfo = age;
+//
+//        if birthDate < 18 {
+//            String tooYoung = jObj.getString("Users must be at least 18 years old.");
+//            }
+//        else
 
+           // Move user to completion page
 
-
-
-
-
-
-}
+            }
+        }
