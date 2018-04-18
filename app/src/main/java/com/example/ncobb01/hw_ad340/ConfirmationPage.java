9748712f313427481ed55ff30ec06da1d62305cb
ncobb01 <ncobb01@gmail.com>
2018-04-17 22:34:17 -0700

    two pages connected (back and forth ability)

--- app/src/main/java/com/example/ncobb01/hw_ad340/ConfirmationPage.java
+++ app/src/main/java/com/example/ncobb01/hw_ad340/ConfirmationPage.java
@@ -1,29 +1,30 @@
 package com.example.ncobb01.hw_ad340;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class ConfirmationPage extends AppCompatActivity {
 
+    private Button btnHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation_page);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+
+        btnHome = (Button) findViewById(R.id.homepage);
+
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(ConfirmationPage.this, MainActivity.class));
             }
         });
-    }
 
-}
+    }}
