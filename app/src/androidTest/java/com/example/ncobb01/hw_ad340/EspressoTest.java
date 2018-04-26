82ef81d62b72cacb97a41c923b9e9e48a00dc8d7
ncobb01 <ncobb01@gmail.com>
2018-04-26 10:43:50 -0700

    current version with tests (all running

--- app/src/androidTest/java/com/example/ncobb01/hw_ad340/EspressoTest.java
+++ app/src/androidTest/java/com/example/ncobb01/hw_ad340/EspressoTest.java
@@ -1,14 +1,14 @@
 package com.example.ncobb01.hw_ad340;
 
-
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-
+import org.hamcrest.Matchers;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
+import android.support.test.espresso.contrib.PickerActions;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
@@ -20,7 +20,9 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
 
 @RunWith(AndroidJUnit4.class)
 public class EspressoTest {
@@ -49,6 +51,11 @@ public class EspressoTest {
     }
 
     @Test
+    public void mainActivityTest2() {
+        onView(withId(R.id.secondActivityBtn)).check(matches(withText(R.string.click_to_see_your_profile)));
+    }
+
+    @Test
     public void entryExample() {
 
         onView(withId(R.id.nameEditText))
@@ -71,6 +78,26 @@ public class EspressoTest {
         onView(withId(R.id.secondActivityBtn)).perform(scrollTo()).perform(click());}
 
 
+
+
+//
+//    @Test
+//    public static void setDate(int datePickerLaunchViewId, int year, int monthOfYear, int dayOfMonth) {
+//        onView(withId(datePickerLaunchViewId)).perform(click());
+//        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(year, monthOfYear, dayOfMonth));
+//        onView(withId(android.R.id.button1)).perform(click());
+//
+//
+//        setDate(R.id.dp, 1994, 1, 1);
+//    }
+
+
+
+
+
+
+
+//
 //    @Test
 //    public void canGoToSecondActivityWithMessage() {
 //        onView(withId(R.id.nameEditText)).perform(typeText("Nathan Cobb"));
@@ -80,9 +107,9 @@ public class EspressoTest {
 //            onView(withId(R.id.secondActivityBtn)).perform(scrollTo(), click());
 //            intended(hasComponent(ConfirmationPage.class.getName()));
 //            intended(hasExtra(Constants.KEY_NAME, "Nathan Cobb"));
-//            intended(hasExtra(Constants.KEY_AGE, 28));
-//            intended(hasExtra(Constants.KEY_OCCUPATION, "Office Manager"));
-//            intended(hasExtra(Constants.KEY_OCCUPATION2, "I like the sun."));
+//
+//           // intended(hasExtra(Constants.KEY_OCCUPATION, "Office Manager"));
+//           // intended(hasExtra(Constants.KEY_OCCUPATION2, "I like the sun."));
 //
 //
 //
