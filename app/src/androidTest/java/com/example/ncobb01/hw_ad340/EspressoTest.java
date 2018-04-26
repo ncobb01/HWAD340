c2f8977990b81f5b50e19df18f16e0781a38cd26
ncobb01 <ncobb01@gmail.com>
2018-04-25 21:07:30 -0700

    current version with first test

--- app/src/androidTest/java/com/example/ncobb01/hw_ad340/EspressoTest.java
+++ app/src/androidTest/java/com/example/ncobb01/hw_ad340/EspressoTest.java
@@ -31,14 +31,14 @@ public class EspressoTest {
     @Test
     public void mainActivityTest() {
         onView(withId(R.id.assignInfo)).check(matches(withText(R.string.nathan_cobb_4_24_18)));
-        onView(withId(R.id.nameEditText)).check(matches(withText(R.string.please_enter_your_name)));
-        onView(withId(R.id.form_birthSpin)).check(matches(withText(R.string.please_select_your_birthdate)));
-
-        // need to add datepicker still!
-
-        onView(withId(R.id.occupationEditText)).check(matches(withText(R.string.please_enter_your_occupation)));
-        onView(withId(R.id.occupation2EditText)).check(matches(withText(R.string.please_describe_yourself)));
-        onView(withId(R.id.loginBtn)).check(matches(withText(R.string.log_in)));
+//        onView(withId(R.id.nameEditText)).check(matches(withText(R.string.please_enter_your_name)));
+//        onView(withId(R.id.form_birthSpin)).check(matches(withText(R.string.please_select_your_birthdate)));
+//
+//        // need to add datepicker still!
+//
+//        onView(withId(R.id.occupationEditText)).check(matches(withText(R.string.please_enter_your_occupation)));
+//        onView(withId(R.id.occupation2EditText)).check(matches(withText(R.string.please_describe_yourself)));
+//        onView(withId(R.id.loginBtn)).check(matches(withText(R.string.log_in)));
 
 
         //onView(withId(R.id.loginBtn)).perform(click()).check(matches(isEnabled()));
@@ -50,25 +50,25 @@ public class EspressoTest {
 
 
 
-    @Test
-    public void canGoToSecondActivityWithMessage() {
-        onView(withId(R.id.nameEditText)).perform(typeText("Nathan Cobb"));
-
-        try {
-            Intents.init();
-            onView(withId(R.id.secondActivityBtn)).perform(scrollTo(), click());
-            intended(hasComponent(ConfirmationPage.class.getName()));
-            intended(hasExtra(Constants.KEY_NAME, "Nathan Cobb"));
-            intended(hasExtra(Constants.KEY_AGE, 28));
-            intended(hasExtra(Constants.KEY_OCCUPATION, "Office Manager"));
-            intended(hasExtra(Constants.KEY_OCCUPATION2, "I like the sun."));
-
-
-
-        } finally {
-            Intents.release();
-        }
-    }
-
+//    @Test
+//    public void canGoToSecondActivityWithMessage() {
+//        onView(withId(R.id.nameEditText)).perform(typeText("Nathan Cobb"));
+//
+//        try {
+//            Intents.init();
+//            onView(withId(R.id.secondActivityBtn)).perform(scrollTo(), click());
+//            intended(hasComponent(ConfirmationPage.class.getName()));
+//            intended(hasExtra(Constants.KEY_NAME, "Nathan Cobb"));
+//            intended(hasExtra(Constants.KEY_AGE, 28));
+//            intended(hasExtra(Constants.KEY_OCCUPATION, "Office Manager"));
+//            intended(hasExtra(Constants.KEY_OCCUPATION2, "I like the sun."));
+//
+//
+//
+//        } finally {
+//            Intents.release();
+//        }
+//    }
+//
 
 }
