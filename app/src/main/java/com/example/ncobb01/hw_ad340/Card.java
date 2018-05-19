184fd3fca9238893a28f6701556da914d96de8f0
ncobb01 <ncobb01@gmail.com>
2018-05-19 14:09:25 -0700

    assign 6 raising cod voc

--- app/src/main/java/com/example/ncobb01/hw_ad340/Card.java
+++ app/src/main/java/com/example/ncobb01/hw_ad340/Card.java
@@ -1,25 +1,9 @@
 package com.example.ncobb01.hw_ad340;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
 
 public class Card {
     private String imgURL;
     private String title;
-
-//    public Card(String imgURL, String title) {
-//        this.imgURL = imgURL;
-//        this.title = title;
 
 
 
