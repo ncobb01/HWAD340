43dec955ce984ff61b9d8f01269fbec05aef78ed
ncobb01 <ncobb01@gmail.com>
2018-05-20 13:11:28 -0700

    assign 6

--- app/src/main/java/com/example/ncobb01/hw_ad340/RecyclerAdapter.java
+++ app/src/main/java/com/example/ncobb01/hw_ad340/RecyclerAdapter.java
@@ -1,81 +1,81 @@
 package com.example.ncobb01.hw_ad340;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ *
+ */
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyHoder>{
+
+    List<Card> list;
+    Context context;
+
+    public RecyclerAdapter(List<Card> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
 //
-//import android.content.Context;
-//import android.support.v7.widget.RecyclerView;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.TextView;
+    @Override
+    public MyHoder onCreateViewHolder(ViewGroup parent, int viewType) {
 //
-//import java.util.List;
-//
-///**
-// *
-// */
-//
-//public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyHoder>{
-//
-////    List<Card> list;
-////    Context context;
-////
-////    public RecyclerAdapter(List<Card> list, Context context) {
-////        this.list = list;
-////        this.context = context;
-////    }
-////
-//    @Override
-//    public MyHoder onCreateViewHolder(ViewGroup parent, int viewType) {
-////
-////        View view = LayoutInflater.from(context).inflate(R.layout.card_layout_main,parent,false);
-////        MyHoder myHoder = new MyHoder(view);
-////
-////
-////        return myHoder;
-////    }
-////
-////    @Override
-////    public void onBindViewHolder(MyHoder holder, int position) {
-////        Card mylist = list.get(position);
-////        holder.name.setText(mylist.getTitle());
-////    }
-////
-////    @Override
-////    public int getItemCount() {
-////
-////        int arr = 0;
-////
-////        try{
-////            if(list.size()==0){
-////
-////                arr = 0;
-////
-////            }
-////            else{
-////
-////                arr=list.size();
-////            }
-////
-////
-////
-////        }catch (Exception e){
-////
-////
-////
-////        }
-////
-////        return arr;
-////
-////    }
-////
-////    class MyHoder extends RecyclerView.ViewHolder{
-////        TextView name,email,address;
-////
-////
-////        public MyHoder(View itemView) {
-////            super(itemView);
-////            name = (TextView) itemView.findViewById(R.id.cardTitle);
-////
-////        }
-////    }
-//
-//}
+        View view = LayoutInflater.from(context).inflate(R.layout.card_layout_main,parent,false);
+        MyHoder myHoder = new MyHoder(view);
+
+
+        return myHoder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyHoder holder, int position) {
+        Card mylist = list.get(position);
+        holder.name.setText(mylist.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+
+        int arr = 0;
+
+        try{
+            if(list.size()==0){
+
+                arr = 0;
+
+            }
+            else{
+
+                arr=list.size();
+            }
+
+
+
+        }catch (Exception e){
+
+
+
+        }
+
+        return arr;
+
+    }
+
+    class MyHoder extends RecyclerView.ViewHolder{
+        TextView name,email,address;
+
+
+        public MyHoder(View itemView) {
+            super(itemView);
+            name = (TextView) itemView.findViewById(R.id.cardTitle);
+
+        }
+    }
+
+}
