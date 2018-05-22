4ab97774818264d10ca8d0bce294bb0bef08a9d0
ncobb01 <ncobb01@gmail.com>
2018-05-22 11:11:49 -0700

    assign 7 errors but all parts included

--- app/src/main/java/com/example/ncobb01/hw_ad340/FragmentC.java
+++ app/src/main/java/com/example/ncobb01/hw_ad340/FragmentC.java
@@ -26,6 +26,14 @@ public class FragmentC extends Fragment {
 
     private static final String TAG = FragmentA.class.getSimpleName();
 
+    public TextView email;
+    public TextView reminderTime;
+    public TextView maxDistance;
+    public TextView gender;
+    public TextView profileType;
+    public TextView ageRange;
+
+
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -36,44 +44,71 @@ public class FragmentC extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        email = findViewById(R.id.email);
+        displayName = findViewById(R.id.displayName);
+        photoUrl = findViewById(R.id.photoUrl);
 
+        new GetUserTask(this, "test@gmail.com").execute();
     }
 
 
-    @Nullable
+    public void updateDatabase(View view) {
+        User fakeNewUser = new User();
+        String email = this.email.getText().toString();
+        if(email.equals("")) {
+            email = "fakeuser@google.com";
+        }
+        fakeNewUser.setEmail(email);
+        fakeNewUser.setReminderTime("12pm");
+        fakeNewUser.setMaxDistance("20");
+        fakeNewUser.setGender("Male");
+        fakeNewUser.setProfileType("Public");
+        fakeNewUser.setAgeRange("20-45");
+
+        new UpdateUserTask(this, fakeNewUser).execute();
+    }
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        Log.i(TAG, "onCreate()");
-
-        return inflater.inflate(R.layout.fragment_c, container, false);
-
-
-
-    }
-
-    private static class SetUserTask extends AsyncTask<Void, Void, User> {
-
-        private WeakReference<Activity> weakActivity;
-        private User user;
-
-        public SetUserTask(Activity activity, User user) {
-            weakActivity = new WeakReference<>(activity);
-            this.user = user;
+    protected User doInBackground(Void... voids) {
+        Activity activity = weakActivity.get();
+        if(activity == null) {
+            return null;
         }
 
-        @Override
-        protected User doInBackground(Void... voids) {
-            Activity activity = weakActivity.get();
-            if(activity == null) {
-                return null;
-            }
+        AppDatabase db = AppDatabaseSingleton.getDatabase(activity.getApplicationContext());
 
-            AppDatabase db = AppDatabaseSingleton.getDatabase(activity.getApplicationContext());
-
-            db.userDao().insertAll(user);
-            return user;
-        }
+        db.userDao().delete(user);
+        return user;
     }
+
+
+
+
+
+
+
+
+
+    @Override
+    protected void onPostExecute(User user) {
+        FragmentC activity = (FragmentC) weakActivity.get();
+        if(user == null || activity == null) {
+            return;
+        }
+
+        activity.email.setText("");
+        activity.reminderTime.setText("");
+        activity.maxDistance.setText("");
+        activity.gender.setText("");
+        activity.profileType.setText("");
+        activity.ageRange.setText("");
+    }
+
+
+
+
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -123,4 +158,87 @@ public class FragmentC extends Fragment {
         Log.i(TAG, "onDetach()");
     }
 
+
+
+
+
+    private static class UpdateUserTask extends AsyncTask<Void, Void, User> {
+
+        private WeakReference<Activity> weakActivity;
+        private User user;
+
+        public UpdateUserTask(Activity activity, User user) {
+            weakActivity = new WeakReference<>(activity);
+            this.user = user;
+        }
+
+        @Override
+        protected User doInBackground(Void... voids) {
+            Activity activity = weakActivity.get();
+            if(activity == null) {
+                return null;
+            }
+
+
+            AppDatabase db = AppDatabaseSingleton.getDatabase(activity.getApplicationContext());
+
+            db.userDao().updateUsers(user);
+            return user;
+        }
+
+        @Override
+        protected void onPostExecute(User user) {
+            RoomPersistenceExampleActivity activity = (RoomPersistenceExampleActivity) weakActivity.get();
+            if(user == null || activity == null) {
+                return;
+            }
+
+            activity.email.setText(user.getEmail());
+            activity.displayName.setText(String.format("%s %s", user.getFirstName(), user.getLastName()));
+            activity.photoUrl.setText(user.getPhotoUrl());
+        }
+    }
+
+    private static class GetUserTask extends AsyncTask<Void, Void, User> {
+
+        private WeakReference<Activity> weakActivity;
+        private String userEmail;
+
+        public GetUserTask(Activity activity, String userEmail) {
+            weakActivity = new WeakReference<>(activity);
+            this.userEmail = userEmail;
+        }
+
+        @Override
+        protected User doInBackground(Void... voids) {
+            Activity activity = weakActivity.get();
+            if(activity == null) {
+                return null;
+            }
+
+            AppDatabase db = AppDatabaseSingleton.getDatabase(activity.getApplicationContext());
+
+            String[] emails = { userEmail };
+
+            List<User> users = db.userDao().loadAllByIds(emails);
+
+            if(users.size() <= 0 || users.get(0) == null) {
+                return null;
+            }
+            return users.get(0);
+        }
+
+        @Override
+        protected void onPostExecute(User user) {
+            RoomPersistenceExampleActivity activity = (RoomPersistenceExampleActivity) weakActivity.get();
+            if(user == null || activity == null) {
+                return;
+            }
+
+            activity.email.setText(user.getEmail());
+            activity.displayName.setText(String.format("%s %s", user.getFirstName(), user.getLastName()));
+            activity.photoUrl.setText(user.getPhotoUrl());
+        }
+    }
 }
+
